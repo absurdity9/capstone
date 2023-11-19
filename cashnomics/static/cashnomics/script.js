e29cb1ee2728ce6ea31913c54d1dfd74cb396aa0
absurdity69 <kfd@outlook.com>
2023-11-19 15:14:44 +0000

    + fix progress bar, styling login/signup pages

--- cashnomics/static/cashnomics/script.js
+++ cashnomics/static/cashnomics/script.js
@@ -1,46 +1,84 @@
-// Get form steps
-const formSteps = document.querySelectorAll('.form_wrap .data_info');
+// Get progress bar element
+const progressBar = document.getElementById('progressBar');
 
-// Get progress bar elements  
-const progressBars = document.querySelectorAll('.header ul li');
+// Get progress pills
+const progressPills = document.querySelectorAll('.nav-pills .nav-link');
 
 // Get buttons
-const nextButtons = document.querySelectorAll('.btn_next');
-const backButtons = document.querySelectorAll('.btn_back');
+const nextButton = document.getElementById('nextBtn');
+const backButton = document.getElementById('prevBtn');
 
 let currentStep = 0;
 
-// Hide all steps
-formSteps.forEach(step => step.style.display = 'none');
-
-// Show first step
-formSteps[0].style.display = 'block';
-
-// Set first progress bar active  
-progressBars[0].classList.add('active');
-
 // Button click handlers
-nextButtons.forEach(btn => {
-  btn.addEventListener('click', nextStep)  
-});
+nextButton.addEventListener('click', nextStep);
+backButton.addEventListener('click', prevStep);
 
-backButtons.forEach(btn => {
-  btn.addEventListener('click', prevStep)
+// Progress pill click handler
+progressPills.forEach((pill, index) => {
+  pill.addEventListener('click', () => {
+    updateStep(index);
+  });
 });
 
 // Next step
 function nextStep() {
-	formSteps[currentStep].style.display = 'none';
-	currentStep++;  
-	formSteps[currentStep].style.display = 'block';
-	progressBars[currentStep].classList.add('active');
+  updateStep(currentStep + 1);
+}
+
+// Previous step
+function prevStep() {
+  updateStep(currentStep - 1);
+}
+
+// Update step
+function updateStep(step) {
+  // Hide current step
+  formSteps[currentStep].classList.remove('show', 'active');
+  progressPills[currentStep].classList.remove('active');
+
+  // Update current step
+  currentStep = step;
+
+  // Show updated step
+  formSteps[currentStep].classList.add('show', 'active');
+  progressPills[currentStep].classList.add('active');
+
+  // Update progress bar
+  const progress = ((currentStep + 1) / formSteps.length) * 100;
+  progressBar.style.width = `${progress}%`;
+  progressBar.setAttribute('aria-valuenow', progress);
+
+  // Enable/disable buttons based on current step
+  if (currentStep === 0) {
+    backButton.disabled = true;
+  } else {
+    backButton.disabled = false;
   }
-  
-  // Previous step
-  function prevStep() {
-	progressBars[currentStep].classList.remove('active');
-	formSteps[currentStep].style.display = 'none';
-	currentStep--;
-	formSteps[currentStep].style.display = 'block'; 
-	progressBars[currentStep].classList.add('active');
+
+  if (currentStep === formSteps.length - 1) {
+    nextButton.innerText = 'Done';
+  } else {
+    nextButton.innerText = 'Next';
   }
+}
+
+// Get form steps
+const formSteps = document.querySelectorAll('.tab-pane');
+
+// Hide all steps
+formSteps.forEach((step, index) => {
+  if (index === 0) {
+    step.classList.add('show', 'active');
+  } else {
+    step.classList.remove('show', 'active');
+  }
+});
+
+// Set initial progress bar width
+progressBar.style.width = '0%';
+
+// Set click event on first progress pill
+progressPills[0].addEventListener('click', () => {
+  updateStep(0);
+});
